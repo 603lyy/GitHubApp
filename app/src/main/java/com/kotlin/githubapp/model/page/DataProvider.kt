b80647fe6381c1e204c7492ffa40cfb93daf8828
Administrator <840567289>
2020-04-24 09:54:29 +0800

    实现通用列表页的model部分

---
+++ app/src/main/java/com/kotlin/githubapp/model/page/DataProvider.kt
@@ -0,0 +1,8 @@
+package com.kotlin.githubapp.model.page
+
+import retrofit2.adapter.rxjava.GitHubPaging
+import rx.Observable
+
+interface DataProvider<DataType> {
+    fun getData(page: Int): Observable<GitHubPaging<DataType>>
+}
---
+++ app/src/main/java/com/kotlin/githubapp/model/repo/RepoModel.kt
@@ -0,0 +1,21 @@
+package com.kotlin.githubapp.model.repo
+
+import com.kotlin.githubapp.model.page.ListPage
+import com.kotlin.githubapp.network.entities.Repository
+import com.kotlin.githubapp.network.entities.User
+import com.kotlin.githubapp.network.services.RepositoryService
+import com.kotlin.githubapp.utils.format
+import retrofit2.adapter.rxjava.GitHubPaging
+import rx.Observable
+import java.util.*
+
+class RepoListPage(val owner: User?): ListPage<Repository>(){
+    override fun getData(page: Int): Observable<GitHubPaging<Repository>> {
+        return if(owner == null){
+            RepositoryService.allRepositories(page, "pushed:<" + Date().format("yyyy-MM-dd")).map { it.paging }
+        } else {
+            RepositoryService.listRepositoriesOfUser(owner.login, page)
+        }
+    }
+
+}
