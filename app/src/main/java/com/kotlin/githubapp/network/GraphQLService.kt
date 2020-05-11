74cfc2fcb2b6abce6de421181a36312ab81ef338
lyy <840567289@qq.com>
2020-05-11 11:27:59 +0800

    实现GraphQLService

---
+++ app/src/main/java/com/kotlin/githubapp/network/GraphQLService.kt
@@ -0,0 +1,18 @@
+package com.kotlin.githubapp.network
+
+import com.apollographql.apollo.ApolloClient
+import com.kotlin.githubapp.network.interceptors.AuthInterceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+
+private const val BASE_URL = "https://api.github.com/graphql"
+
+val apolloClient by lazy {
+    ApolloClient.builder()
+        .serverUrl(BASE_URL)
+        .okHttpClient(
+            OkHttpClient.Builder().addInterceptor(AuthInterceptor())
+            .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BASIC))
+            .build())
+        .build()
+}
