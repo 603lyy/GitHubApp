83ed5a9d5a084bf67ba2057b05d6909db5ef92d7
lyy <840567289@qq.com>
2020-05-11 16:15:26 +0800

    更换token

--- app/src/main/java/com/kotlin/githubapp/settings/Configs.kt
+++ app/src/main/java/com/kotlin/githubapp/settings/Configs.kt
@@ -12,8 +12,7 @@ object Configs {
         const val clientSecret = "4874602afa6106f06d8d86ac70d000f1c0fc1d23"
         const val note = "kotliner.cn"
         const val noteUrl = "http://www.kotliner.cn"
-        const val accessToken = "eb9fc35224ac0cf59cbd55e8e482756bc7597f8f "
-
+        const val accessToken = "7d8c89acd57ded981295083332b6a8a9f8d10ba0 "
         val fingerPrint by lazy {
             (AppContext.deviceId + clientId).also { logger.info("fingerPrint: "+it) }
         }
