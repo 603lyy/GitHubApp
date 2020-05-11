fcd1d58720ddc8520a02f5f3e454d6f615de890a
lyy <840567289@qq.com>
2020-05-11 10:11:50 +0800

    测试分支

--- app/src/main/java/com/kotlin/githubapp/view/fragment/RepoListFragment.kt
+++ app/src/main/java/com/kotlin/githubapp/view/fragment/RepoListFragment.kt
@@ -1,7 +1,5 @@
 package com.kotlin.githubapp.view.fragment
 
-import android.content.Intent
-import android.content.Intent.parseUri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
